1e19b09a8ae9ffcedb5a24211ba51a61a747a4ba
Dan Cross <dancross96@gmail.com>
2022-08-02 15:48:50 -0500

    used the && operator to dry the code.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Biology_Practice
+As of right now, the app can only run 60 base pairs at once. It will tell you the location of each start/stop codons in the base pairs. 
--- logic.rb
+++ logic.rb
@@ -11,70 +11,118 @@ File.open("sequence.txt", "r") do |file|
 end
 p sequence_array
 
-index = 0
 
+index = 0
 while index < sequence_array.length
-  if sequence_array[index] == "a"
-    if sequence_array[index + 1] == "u"
-      if sequence_array[index + 2] == "g"
-        p "start codon at position #{index}"
-        index += 1
-      end
-    else
-      index += 1
-    end
+  if sequence_array[index] == "a" && sequence_array[index + 1] == "u" && sequence_array[index + 2] == "g"
+    p "start codon at position #{index}"
+    index += 1
   else
     index += 1
   end
 end
 
-index = 0
 
+# index = 0
+
+# while index < sequence_array.length
+#   if sequence_array[index] == "a"
+#     if sequence_array[index + 1] == "u"
+#       if sequence_array[index + 2] == "g"
+#         p "start codon at position #{index}"
+#         index += 1
+#       end
+#     else
+#       index += 1
+#     end
+#   else
+#     index += 1
+#   end
+# end
+
+
+index = 0
 while index < sequence_array.length
-  if sequence_array[index] == "u"
-    if sequence_array[index + 1] == "a"
-      if sequence_array[index + 2] == "a"
-        p "stop codon at position #{index}"
-        index += 1
-      end
-    else
-      index += 1
-    end
+  if sequence_array[index] == "u" && sequence_array[index + 1] == "a" && sequence_array[index + 2] == "a"
+    p "stop codon at position #{index}"
+    index += 1
   else
     index += 1
   end
 end
 
-index = 0
+# index = 0
 
+# while index < sequence_array.length
+#   if sequence_array[index] == "u"
+#     if sequence_array[index + 1] == "a"
+#       if sequence_array[index + 2] == "a"
+#         p "stop codon at position #{index}"
+#         index += 1
+#       end
+#     else
+#       index += 1
+#     end
+#   else
+#     index += 1
+#   end
+# end
+
+
+
+index = 0
 while index < sequence_array.length
-  if sequence_array[index] == "u"
-    if sequence_array[index + 1] == "a"
-      if sequence_array[index + 2] == "g"
-        p "stop codon at position #{index}"
-        index += 1
-      end
-    else
-      index += 1
-    end
+  if sequence_array[index] == "u" && sequence_array[index + 1] == "a" && sequence_array[index + 2] == "g"
+    p "stop codon at position #{index}"
+    index += 1
   else
     index += 1
   end
 end
 
-index = 0
 
+# index = 0
+
+# while index < sequence_array.length
+#   if sequence_array[index] == "u"
+#     if sequence_array[index + 1] == "a"
+#       if sequence_array[index + 2] == "g"
+#         p "stop codon at position #{index}"
+#         index += 1
+#       end
+#     else
+#       index += 1
+#     end
+#   else
+#     index += 1
+#   end
+# end
+
+
+index = 0
 while index < sequence_array.length
-  if sequence_array[index] == "a"
-    if sequence_array[index + 1] == "g"
-      if sequence_array[index + 2] == "a"
-        p "stop codon at position #{index}"
-        index += 1
-      end
-    else
-      index += 1
-    end
+  if sequence_array[index] == "a" && sequence_array[index + 1] == "g" && sequence_array[index + 2] == "a"
+    p "stop codon at position #{index}"
+    index += 1
   else
     index += 1
   end
 end
+
+
+# index = 0
+
+# while index < sequence_array.length
+#   if sequence_array[index] == "a"
+#     if sequence_array[index + 1] == "g"
+#       if sequence_array[index + 2] == "a"
+#         p "stop codon at position #{index}"
+#         index += 1
+#       end
+#     else
+#       index += 1
+#     end
+#   else
+#     index += 1
+#   end
+# end
